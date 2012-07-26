ed2b4f6572360f16cd7cf3ff1ce02f04d307156f
acoogan <mogget03@gmail>
2012-07-25 22:02:58 -0500

    Rough draft of the interface.  Learned some basic CSS.  Can add stops with appropriately numbered icons!

---
+++ testjs.js
@@ -0,0 +1,12 @@
+// Set up the map with these options in the specified place.  Load it when the window loads.
+function initialize() {
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(-34.397, 150.644),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map_canvas'),
+        mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
